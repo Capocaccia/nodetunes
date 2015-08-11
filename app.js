363db3f3482b8c149d65a55b7011bd752b66033b
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-08-11 13:58:47 -0500

    extracted routes

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var newartist = require('./routes/newartists');
+var search = require('./routes/search');
+var aindex = require('./routes/artistindex');
 
 require('./lib/mongodb');
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-  res.render('newartists');
-})
-// I have this rendering newartists right now so it just doesnt display blank at the root route
-// It will be refactored to display each artists info
-
-app.get('/newartist', function (req, res) {
-  res.render('newartists')
-})
+app.use('/', index);
+app.use('/newartists', newartist);
+app.use('/search', search);
+app.use('/aindex', aindex);
 
 app.post('/artist/create', function (req, res){
   var collection = global.db.collection('musicinfo');
-
   collection.save(req.body, function () {
     res.redirect('/')
   });
 })
+
+app.get('/search/results', function (req, res) {
+  res.write('Hello World')
+  console.log(req.params)
+})
+
 
 var port = process.env.PORT || 3000;
 
