dd1978e75b1d6488f2874bb84f1e5afcb1240080
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-08-13 15:06:03 -0500

    added search logic

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    console.log( "ready!" );
-});
+$('.btn_search').on('click', function() {
+    var artistName = $('input[name="name"]').val();
+    var nameObj = {name: artistName}
+    $.get('search/results', nameObj, function(artists) {
+      appendArtists(artists)
+    })
+    event.preventDefault();
+})
+
+function appendArtists(artists) {
+  console.log(artists)
+  //html for each artist
+  artists.forEach(function(artist) {
+    var html = '<option value=' + artist._id + ' >' + artist.name + '</option>';
+  })
+};
